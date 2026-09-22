1f09900eb96fc9cb5706f8451ea3a61c6114858c
hagbard851 <cruznogu@gmail.com>
2026-09-22 14:22:53 -0600

    v15.2: trigger installed-app update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "butsukari-shell-v15.1";
+const CACHE_NAME = "butsukari-shell-v15.2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
